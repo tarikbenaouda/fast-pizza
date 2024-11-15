3ac22e51dec14038081205c04051539fd48450c4
tarikbenaouda <benaouda210@gmail.com>
2024-11-15 17:39:24 +0100

    Update Style in CreateUser

--- src/featuers/user/CreateUser.jsx
+++ src/featuers/user/CreateUser.jsx
@@ -19,7 +19,7 @@ function CreateUser() {
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        className="input w-72"
+        className="input mb-4 w-72"
       />
 
       {username !== '' && (
