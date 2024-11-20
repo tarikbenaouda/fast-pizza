7555375d33a5daa524cc1fb8cbc698f3514c6be1
tarikbenaouda <benaouda210@gmail.com>
2024-11-20 22:21:58 +0100

    Add Ingredients fetching --> Order.jsx

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 // Test ID: IIDSAT
 
-import { useLoaderData } from 'react-router-dom';
+import { useFetcher, useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
@@ -9,6 +9,7 @@ import {
   formatDate,
 } from '../../utils/helpers';
 import OrderItem from './OrderItem';
+import { useEffect } from 'react';
 
 // const order = {
 //   id: "ABCDEF",
@@ -48,6 +49,13 @@ import OrderItem from './OrderItem';
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(() => {
+    console.log('useEffect');
+    if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+  }, [fetcher]);
   const {
     id,
     status,
@@ -87,7 +95,15 @@ function Order() {
 
       <ul className="dive-stone-200 divide-y border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === 'loading'}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
